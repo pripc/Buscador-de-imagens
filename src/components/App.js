c790d632f43141acc3fb1a01b5e0d74d18f1c939
pripc <117996744+pripc@users.noreply.github.com>
2023-02-08 10:28:19 -0300

    Fazendo as imagens aparecerem após busca

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,24 @@
 import React from 'react';
 import axios from 'axios'
 import Searchinput from './Searchinput'
+import Imagelist from './Imagelist'
 
 class App extends React.Component{
-onSearchSubmit = (entry) => {
-    console.log(entry)
-    axios.get(`https://pixabay.com/api/?key=33459439-d4710cf91085425a589972622&q=${entry}&image_type=photo`)
-}
+    
+    state ={ images: [] }
+    
+    onSearchSubmit = async (entry) => {
+        const response = await axios.get(`https://pixabay.com/api/?key=33459439-d4710cf91085425a589972622&q=${entry}&image_type=photo`)
+        this.setState({images:response.data.hits})
+    }
 
     render(){
         return(
             <div className="ui container" style={{marginTop:'30px'}}>
             <Searchinput onSearchSubmit={this.onSearchSubmit} />
+            <Imagelist images={this.state.images}/>
         </div>
         )
     }
 }
-
 export default App
